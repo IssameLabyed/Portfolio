cc3a577225bcaa1cd114e0c9d33b9c38450ce7c4
picola07 <99802423+picola07@users.noreply.github.com>
2025-09-14 18:13:03 +0000

    koji

--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // base: '/Portfolio/',
-  base: '/Portfolio/', 
-  publicDir: 'public',
-})
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
-// import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
+// // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
-//   base: '/Portfolio/', 
-//   publicDir: 'public',
-//   build: {
-//     assetsDir: 'assets',
-//     rollupOptions: {
-//       output: {
-//         assetFileNames: (assetInfo) => {
-//           if (assetInfo.name.endsWith('.jpg') || assetInfo.names.endsWith('.png'))
-//           {return 'images/[name][ext]';}
-//           return 'assets/[name] - [hash][ext]';
-//         },
-//       },
-//     },
-//   },
-//   optimizeDeps: {
-//     esbuildOptions: {
-//       define: {
-//         global: 'globalThis',
-//       },
-//       plugins: [
-//         NodeGlobalsPolyfillPlugin({
-//           process: true,
-//           buffer: true,
-//         }),
-//       ],
-//     },
-//   },
-//   resolve: {
-//     alias: {
-//       crypto: 'crypto-browserify',
-//     },
-//   },
+//   // base: '/Portfolio/',
 // })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Portfolio/', 
+  publicDir: 'public',
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+    },
+  },
+})
