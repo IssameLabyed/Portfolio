b67bcb604373173021292289110dc73c8325d181
picola07 <99802423+picola07@users.noreply.github.com>
2025-09-16 02:52:33 +0000

    CV Two languages

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -6,7 +6,7 @@ export interface ISkill {
 
   export const skills: ISkill[] = [
     { name: 'HTML/CSS/JS', category: 'Web', icon: 'code' }, 
-    { name: 'React', category: 'Web', icon: 'sparkles' }, 
+    // { name: 'React', category: 'Web', icon: 'sparkles' }, 
     { name: 'Tailwind', category: 'Web', icon: 'adjustments-horizontal' }, 
     { name: 'Flutter', category: 'Mobile', icon: 'device-phone-mobile' }, 
     { name: 'Python', category: 'Programming', icon: 'code' }, 
@@ -16,7 +16,9 @@ export interface ISkill {
     { name: 'TypeScript', category: 'Programming', icon: 'document-text' }, 
     { name: 'Docker', category: 'DevOps', icon: 'document-text' }, 
     { name: 'MariaDB', category: 'Databases', icon: 'server' }, 
+    { name: 'MySQl', category: 'Databases', icon: 'server' }, 
     { name: 'C++', category: 'Programming', icon: 'code' }, 
+    { name: 'C#', category: 'Programming', icon: 'code' }, 
     { name: 'C', category: 'Programming', icon: 'code' }, 
     { name: 'Shell', category: 'DevOps', icon: 'terminal' }, 
   ];
